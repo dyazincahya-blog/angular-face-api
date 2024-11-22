8a012a333feac773600b5c745f6e9c789311d8db
kang cahya <cahyadyazin@yahoo.com>
2024-11-22 14:18:52 +0700

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,3 @@
 import { Routes } from '@angular/router';
-import { FaceApiComponent } from './face-api/face-api.component';
 
-export const routes: Routes = [
-  {
-    path: 'face-api',
-    component: FaceApiComponent,
-  },
-];
+export const routes: Routes = [];
